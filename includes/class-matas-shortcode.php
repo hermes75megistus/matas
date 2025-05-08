0b1e96d1465cad73fc03772d2ae2f9961949ecbf
hermes75megistus <hermes@insangram.com>
2025-05-08 19:39:18 +0300

    Update class-matas-shortcode.php

--- includes/class-matas-shortcode.php
+++ includes/class-matas-shortcode.php
@@ -1,1 +1,92 @@
- 
+<?php
+class Matas_Shortcode {
+    private $plugin_name;
+    private $version;
+    private $calculator;
+
+    public function __construct($plugin_name, $version) {
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
+        
+        // Calculator sınıfını yükle
+        require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-matas-calculator.php';
+        $this->calculator = new Matas_Calculator($plugin_name, $version);
+    }
+    
+    /**
+     * Frontend stil dosyalarını ekler
+     */
+    public function enqueue_styles() {
+        wp_enqueue_style($this->plugin_name . '-calculator', MATAS_PLUGIN_URL . 'public/css/style.css', array(), $this->version, 'all');
+    }
+    
+    /**
+     * Frontend script dosyalarını ekler
+     */
+    public function enqueue_scripts() {
+        wp_enqueue_script($this->plugin_name . '-constants', MATAS_PLUGIN_URL . 'public/js/constants.js', array('jquery'), $this->version, false);
+        wp_enqueue_script($this->plugin_name . '-functions', MATAS_PLUGIN_URL . 'public/js/functions.js', array('jquery', $this->plugin_name . '-constants'), $this->version, false);
+        wp_enqueue_script($this->plugin_name . '-calculator', MATAS_PLUGIN_URL . 'public/js/calculator.js', array('jquery', $this->plugin_name . '-functions'), $this->version, false);
+        
+        wp_localize_script($this->plugin_name . '-calculator', 'matas_ajax', array(
+            'ajax_url' => admin_url('admin-ajax.php'),
+            'nonce' => wp_create_nonce('matas_calculator_nonce')
+        ));
+    }
+    
+    /**
+     * Maaş hesaplama formunu gösteren kısa kodu işler
+     *
+     * @param array $atts Kısa kod parametreleri
+     * @return string HTML çıktısı
+     */
+    public function display_calculator($atts) {
+        // Kısa kod parametrelerini birleştir
+        $atts = shortcode_atts(array(
+            'baslik' => 'Memur Maaş Hesaplama',
+            'stil' => 'modern',
+        ), $atts, 'matas_hesaplama');
+        
+        // Çıktı tamponlamasını başlat
+        ob_start();
+        
+        // Form şablonunu dahil et
+        include MATAS_PLUGIN_DIR . 'public/partials/calculator-form.php';
+        
+        // Tamponu döndür
+        return ob_get_clean();
+    }
+    
+    /**
+     * Maaş hesaplama işlemini gerçekleştirir (AJAX)
+     */
+    public function calculate_salary() {
+        // Nonce kontrolü
+        check_ajax_referer('matas_calculator_nonce', 'nonce');
+        
+        // Form verilerini al
+        $params = array();
+        $form_fields = array(
+            'unvan', 'derece', 'kademe', 'hizmet_yili', 'medeni_hal', 'es_calisiyor',
+            'cocuk_sayisi', 'cocuk_06', 'engelli_cocuk', 'ogrenim_cocuk', 'egitim_durumu',
+            'dil_seviyesi', 'dil_kullanimi', 'gorev_tazminati', 'gelistirme_odenegi',
+            'asgari_gecim_indirimi', 'kira_yardimi', 'sendika_uyesi'
+        );
+        
+        foreach ($form_fields as $field) {
+            if (isset($_POST[$field])) {
+                $params[$field] = sanitize_text_field($_POST[$field]);
+            }
+        }
+        
+        // Hesaplama işlemini yap
+        $result = $this->calculator->calculate_salary($params);
+        
+        // Sonucu döndür
+        if ($result['success']) {
+            wp_send_json_success($result);
+        } else {
+            wp_send_json_error($result);
+        }
+    }
+}
