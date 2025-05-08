08969fc73bfca896ce9b38bb43c926a0b7c27b52
hermes75megistus <hermes@insangram.com>
2025-05-08 19:53:46 +0300

    Update class-matas.php

--- includes/class-matas.php
+++ includes/class-matas.php
@@ -32,7 +32,23 @@ class Matas {
         // AJAX işleyicileri
         $this->loader->add_action('wp_ajax_matas_save_katsayilar', $plugin_admin, 'save_katsayilar');
         $this->loader->add_action('wp_ajax_matas_save_unvan', $plugin_admin, 'save_unvan');
-        // Diğer AJAX işleyicileri...
+        $this->loader->add_action('wp_ajax_matas_delete_unvan', $plugin_admin, 'delete_unvan');
+        $this->loader->add_action('wp_ajax_matas_get_unvan', $plugin_admin, 'get_unvan');
+        
+        $this->loader->add_action('wp_ajax_matas_save_gosterge', $plugin_admin, 'save_gosterge');
+        $this->loader->add_action('wp_ajax_matas_get_gosterge', $plugin_admin, 'get_gosterge');
+        $this->loader->add_action('wp_ajax_matas_delete_gosterge', $plugin_admin, 'delete_gosterge');
+        $this->loader->add_action('wp_ajax_matas_load_default_gostergeler', $plugin_admin, 'load_default_gostergeler');
+        
+        $this->loader->add_action('wp_ajax_matas_save_vergi_dilimi', $plugin_admin, 'save_vergi_dilimi');
+        $this->loader->add_action('wp_ajax_matas_get_vergi_dilimi', $plugin_admin, 'get_vergi_dilimi');
+        $this->loader->add_action('wp_ajax_matas_delete_vergi_dilimi', $plugin_admin, 'delete_vergi_dilimi');
+        $this->loader->add_action('wp_ajax_matas_load_default_vergiler', $plugin_admin, 'load_default_vergiler');
+        
+        $this->loader->add_action('wp_ajax_matas_save_sosyal_yardim', $plugin_admin, 'save_sosyal_yardim');
+        $this->loader->add_action('wp_ajax_matas_get_sosyal_yardim', $plugin_admin, 'get_sosyal_yardim');
+        $this->loader->add_action('wp_ajax_matas_delete_sosyal_yardim', $plugin_admin, 'delete_sosyal_yardim');
+        $this->loader->add_action('wp_ajax_matas_load_default_sosyal_yardimlar', $plugin_admin, 'load_default_sosyal_yardimlar');
     }
     
     private function define_public_hooks() {
