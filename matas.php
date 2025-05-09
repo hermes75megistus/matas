87705126610f6ea3057110263f430186051db160
hermes75megistus <hermes@insangram.com>
2025-05-09 19:27:27 +0300

    Update matas.php

--- matas.php
+++ matas.php
@@ -8,6 +8,8 @@
  * Author URI: https://example.com
  * Text Domain: matas
  * Domain Path: /languages
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
 // Doğrudan erişimi engelle
@@ -26,15 +28,23 @@ define('MATAS_PLUGIN_URL', plugin_dir_url(__FILE__));
 function activate_matas() {
     require_once MATAS_PLUGIN_DIR . 'includes/class-matas-activator.php';
     Matas_Activator::activate();
+    flush_rewrite_rules();
 }
 
 function deactivate_matas() {
     require_once MATAS_PLUGIN_DIR . 'includes/class-matas-deactivator.php';
     Matas_Deactivator::deactivate();
+    flush_rewrite_rules();
 }
 
 register_activation_hook(__FILE__, 'activate_matas');
 register_deactivation_hook(__FILE__, 'deactivate_matas');
+
+// Dil dosyalarını yükle
+function matas_load_textdomain() {
+    load_plugin_textdomain('matas', false, dirname(plugin_basename(__FILE__)) . '/languages');
+}
+add_action('plugins_loaded', 'matas_load_textdomain');
 
 // Ana eklenti sınıfını içe aktar
 require_once MATAS_PLUGIN_DIR . 'includes/class-matas.php';
@@ -45,3 +55,18 @@ function run_matas() {
     $plugin->run();
 }
 run_matas();
+
+// Kısa kod önizlemesi için stil
+function matas_admin_styles() {
+    wp_enqueue_style('matas-admin-preview', MATAS_PLUGIN_URL . 'admin/css/preview.css', array(), MATAS_VERSION, 'all');
+}
+add_action('admin_enqueue_scripts', 'matas_admin_styles');
+
+// Ayarlar sayfasına link ekle
+function matas_add_settings_link($links) {
+    $settings_link = '<a href="' . admin_url('admin.php?page=matas') . '">' . __('Ayarlar', 'matas') . '</a>';
+    array_unshift($links, $settings_link);
+    return $links;
+}
+$plugin_basename = plugin_basename(__FILE__);
+add_filter("plugin_action_links_$plugin_basename", 'matas_add_settings_link');
