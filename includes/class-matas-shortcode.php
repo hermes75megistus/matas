207c7aefaeaa6b0ba2e3be452bb29b83f5581f72
hermes75megistus <hermes@insangram.com>
2025-05-09 19:24:47 +0300

    Update class-matas-shortcode.php

--- includes/class-matas-shortcode.php
+++ includes/class-matas-shortcode.php
@@ -1,9 +1,39 @@
 <?php
+/**
+ * Maaş hesaplama kısa kodu sınıfı
+ * 
+ * @package MATAS
+ * @since 1.0.0
+ */
+
 class Matas_Shortcode {
+    /**
+     * Eklenti ismi
+     *
+     * @var string
+     */
     private $plugin_name;
+    
+    /**
+     * Eklenti versiyonu
+     *
+     * @var string
+     */
     private $version;
+    
+    /**
+     * Hesaplama sınıfı
+     *
+     * @var Matas_Calculator
+     */
     private $calculator;
 
+    /**
+     * Sınıfı başlat
+     *
+     * @param string $plugin_name Eklenti ismi
+     * @param string $version Eklenti versiyonu
+     */
     public function __construct($plugin_name, $version) {
         $this->plugin_name = $plugin_name;
         $this->version = $version;
@@ -17,20 +47,50 @@ class Matas_Shortcode {
      * Frontend stil dosyalarını ekler
      */
     public function enqueue_styles() {
-        wp_enqueue_style($this->plugin_name . '-calculator', MATAS_PLUGIN_URL . 'public/css/style.css', array(), $this->version, 'all');
+        $suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
+        wp_enqueue_style(
+            $this->plugin_name . '-calculator', 
+            MATAS_PLUGIN_URL . 'public/css/style' . $suffix . '.css', 
+            array(), 
+            $this->version, 
+            'all'
+        );
     }
     
     /**
      * Frontend script dosyalarını ekler
      */
     public function enqueue_scripts() {
-        wp_enqueue_script($this->plugin_name . '-constants', MATAS_PLUGIN_URL . 'public/js/constants.js', array('jquery'), $this->version, false);
-        wp_enqueue_script($this->plugin_name . '-functions', MATAS_PLUGIN_URL . 'public/js/functions.js', array('jquery', $this->plugin_name . '-constants'), $this->version, false);
-        wp_enqueue_script($this->plugin_name . '-calculator', MATAS_PLUGIN_URL . 'public/js/calculator.js', array('jquery', $this->plugin_name . '-functions'), $this->version, false);
+        $suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
+        wp_enqueue_script(
+            $this->plugin_name . '-constants', 
+            MATAS_PLUGIN_URL . 'public/js/constants' . $suffix . '.js', 
+            array('jquery'), 
+            $this->version, 
+            true
+        );
+        
+        wp_enqueue_script(
+            $this->plugin_name . '-functions', 
+            MATAS_PLUGIN_URL . 'public/js/functions' . $suffix . '.js', 
+            array('jquery', $this->plugin_name . '-constants'), 
+            $this->version, 
+            true
+        );
+        
+        wp_enqueue_script(
+            $this->plugin_name . '-calculator', 
+            MATAS_PLUGIN_URL . 'public/js/calculator' . $suffix . '.js', 
+            array('jquery', $this->plugin_name . '-functions'), 
+            $this->version, 
+            true
+        );
         
         wp_localize_script($this->plugin_name . '-calculator', 'matas_ajax', array(
             'ajax_url' => admin_url('admin-ajax.php'),
-            'nonce' => wp_create_nonce('matas_calculator_nonce')
+            'nonce' => wp_create_nonce('matas_calculator_nonce'),
+            'current_year' => date('Y'),
+            'plugin_url' => MATAS_PLUGIN_URL,
         ));
     }
     
@@ -43,9 +103,15 @@ class Matas_Shortcode {
     public function display_calculator($atts) {
         // Kısa kod parametrelerini birleştir
         $atts = shortcode_atts(array(
-            'baslik' => 'Memur Maaş Hesaplama',
+            'baslik' => __('Memur Maaş Hesaplama', 'matas'),
             'stil' => 'modern',
         ), $atts, 'matas_hesaplama');
+        
+        // CSS sınıfları oluştur
+        $container_class = 'matas-container';
+        if (!empty($atts['stil'])) {
+            $container_class .= ' matas-' . sanitize_html_class($atts['stil']);
+        }
         
         // Çıktı tamponlamasını başlat
         ob_start();
@@ -62,7 +128,13 @@ class Matas_Shortcode {
      */
     public function calculate_salary() {
         // Nonce kontrolü
-        check_ajax_referer('matas_calculator_nonce', 'nonce');
+        if (!check_ajax_referer('matas_calculator_nonce', 'nonce', false)) {
+            wp_send_json_error(array(
+                'success' => false,
+                'message' => __('Güvenlik doğrulaması başarısız!', 'matas')
+            ));
+            return;
+        }
         
         // Form verilerini al ve sanitize et
         $params = array();
@@ -77,26 +149,44 @@ class Matas_Shortcode {
             if (isset($_POST[$field])) {
                 if (in_array($field, array('unvan', 'medeni_hal', 'es_calisiyor', 'egitim_durumu', 'dil_seviyesi', 'dil_kullanimi'))) {
                     $params[$field] = sanitize_text_field($_POST[$field]);
-                } elseif (in_array($field, array('derece', 'kademe', 'hizmet_yili', 'cocuk_sayisi', 'cocuk_06', 'engelli_cocuk', 'ogrenim_cocuk',
-                                             'gorev_tazminati', 'gelistirme_odenegi', 'asgari_gecim_indirimi', 'kira_yardimi', 'sendika_uyesi'))) {
+                } elseif (in_array($field, array('derece', 'kademe', 'hizmet_yili', 'cocuk_sayisi', 'cocuk_06', 'engelli_cocuk', 'ogrenim_cocuk'))) {
                     $params[$field] = intval($_POST[$field]);
+                } elseif (in_array($field, array('gorev_tazminati', 'gelistirme_odenegi', 'asgari_gecim_indirimi', 'kira_yardimi', 'sendika_uyesi'))) {
+                    $params[$field] = (isset($_POST[$field]) && $_POST[$field] == '1') ? 1 : 0;
                 }
             }
         }
         
-        // Hesaplama işlemini yap
-        $result = $this->calculator->calculate_salary($params);
-        
-        // Sonucu döndür
-        if ($result['success']) {
-            wp_send_json_success($result);
-        } else {
-            wp_send_json_error(array(
-                'success' => false,
-                'message' => $result['message']
-            ));
+        // Veri doğrulama
+        $required_fields = array('unvan', 'derece', 'kademe', 'hizmet_yili');
+        foreach ($required_fields as $field) {
+            if (!isset($params[$field]) || $params[$field] === '') {
+                wp_send_json_error(array(
+                    'success' => false,
+                    'message' => __('Lütfen tüm zorunlu alanları doldurunuz.', 'matas')
+                ));
+                return;
+            }
         }
         
-        wp_die(); // AJAX işlemini sonlandır
+        // Hesaplama işlemini yap
+        try {
+            $result = $this->calculator->calculate_salary($params);
+            
+            // Sonucu döndür
+            if ($result['success']) {
+                wp_send_json_success($result);
+            } else {
+                wp_send_json_error(array(
+                    'success' => false,
+                    'message' => $result['message']
+                ));
+            }
+        } catch (Exception $e) {
+            wp_send_json_error(array(
+                'success' => false,
+                'message' => $e->getMessage()
+            ));
+        }
     }
 }
