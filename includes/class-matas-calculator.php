54a93ca08b5180e8b8dee5e24bc796d2196e5f89
hermes75megistus <hermes@insangram.com>
2025-05-09 06:34:09 +0300

    Update class-matas-calculator.php

--- includes/class-matas-calculator.php
+++ includes/class-matas-calculator.php
@@ -26,6 +26,16 @@ class Matas_Calculator {
             "SELECT * FROM {$wpdb->prefix}matas_katsayilar WHERE aktif = 1 ORDER BY id DESC LIMIT 1",
             ARRAY_A
         );
+        
+        // Aktif katsayı yoksa varsayılan değerleri kullan
+        if (!$this->katsayilar) {
+            $this->katsayilar = array(
+                'donem' => date('Y') . ' Ocak-Haziran',
+                'aylik_katsayi' => 0.354507,
+                'taban_katsayi' => 7.715,
+                'yan_odeme_katsayi' => 0.0354507
+            );
+        }
 
         // Unvanları yükle
         $this->unvanlar = $wpdb->get_results(
@@ -46,16 +56,47 @@ class Matas_Calculator {
         );
 
         // Vergi dilimlerini yükle
+        $current_year = date('Y');
         $this->vergiler = $wpdb->get_results(
-            "SELECT * FROM {$wpdb->prefix}matas_vergiler WHERE yil = " . date('Y'),
+            $wpdb->prepare(
+                "SELECT * FROM {$wpdb->prefix}matas_vergiler WHERE yil = %d ORDER BY dilim ASC",
+                $current_year
+            ),
             ARRAY_A
         );
+        
+        // Vergi dilimleri yoksa varsayılan değerleri kullan
+        if (empty($this->vergiler)) {
+            $this->vergiler = array(
+                array('yil' => $current_year, 'dilim' => 1, 'alt_limit' => 0, 'ust_limit' => 70000, 'oran' => 15),
+                array('yil' => $current_year, 'dilim' => 2, 'alt_limit' => 70000, 'ust_limit' => 150000, 'oran' => 20),
+                array('yil' => $current_year, 'dilim' => 3, 'alt_limit' => 150000, 'ust_limit' => 550000, 'oran' => 27),
+                array('yil' => $current_year, 'dilim' => 4, 'alt_limit' => 550000, 'ust_limit' => 1900000, 'oran' => 35),
+                array('yil' => $current_year, 'dilim' => 5, 'alt_limit' => 1900000, 'ust_limit' => 0, 'oran' => 40),
+            );
+        }
 
         // Sosyal yardımları yükle
         $this->sosyal_yardimlar = $wpdb->get_results(
-            "SELECT * FROM {$wpdb->prefix}matas_sosyal_yardimlar WHERE yil = " . date('Y'),
+            $wpdb->prepare(
+                "SELECT * FROM {$wpdb->prefix}matas_sosyal_yardimlar WHERE yil = %d",
+                $current_year
+            ),
             ARRAY_A
         );
+        
+        // Sosyal yardımlar yoksa varsayılan değerleri kullan
+        if (empty($this->sosyal_yardimlar)) {
+            $this->sosyal_yardimlar = array(
+                array('yil' => $current_year, 'tip' => 'aile_yardimi', 'adi' => 'Aile Yardımı', 'tutar' => 1200),
+                array('yil' => $current_year, 'tip' => 'cocuk_normal', 'adi' => 'Çocuk Yardımı', 'tutar' => 150),
+                array('yil' => $current_year, 'tip' => 'cocuk_0_6', 'adi' => '0-6 Yaş Çocuk Yardımı', 'tutar' => 300),
+                array('yil' => $current_year, 'tip' => 'cocuk_engelli', 'adi' => 'Engelli Çocuk Yardımı', 'tutar' => 600),
+                array('yil' => $current_year, 'tip' => 'cocuk_ogrenim', 'adi' => 'Öğrenim Çocuk Yardımı', 'tutar' => 250),
+                array('yil' => $current_year, 'tip' => 'kira_yardimi', 'adi' => 'Kira Yardımı', 'tutar' => 2000),
+                array('yil' => $current_year, 'tip' => 'sendika_yardimi', 'adi' => 'Sendika Yardımı', 'tutar' => 500),
+            );
+        }
     }
 
     /**
@@ -99,10 +140,79 @@ class Matas_Calculator {
         }
 
         if (!$gosterge_puani) {
-            return array(
-                'success' => false,
-                'message' => 'Gösterge puanı bulunamadı!',
+            // Gösterge puanı bulunamadıysa constants.js'deki tabloya bakalım
+            $gosterge_puani_key = $params['derece'] . '-' . $params['kademe'];
+            $default_gosterge_puanlari = array(
+                // 1. Derece
+                '1-1' => 1320, '1-2' => 1380, '1-3' => 1440, '1-4' => 1500, '1-5' => 1560, 
+                '1-6' => 1620, '1-7' => 1680, '1-8' => 1740,
+                
+                // 2. Derece
+                '2-1' => 1155, '2-2' => 1210, '2-3' => 1265, '2-4' => 1320, '2-5' => 1380, 
+                '2-6' => 1440, '2-7' => 1500, '2-8' => 1560,
+                
+                // 3. Derece
+                '3-1' => 1020, '3-2' => 1065, '3-3' => 1110, '3-4' => 1155, '3-5' => 1210, 
+                '3-6' => 1265, '3-7' => 1320, '3-8' => 1380, '3-9' => 1440,
+                
+                // 4. Derece
+                '4-1' => 915, '4-2' => 950, '4-3' => 985, '4-4' => 1020, '4-5' => 1065, 
+                '4-6' => 1110, '4-7' => 1155, '4-8' => 1210, '4-9' => 1265,
+                
+                // 5. Derece
+                '5-1' => 835, '5-2' => 870, '5-3' => 905, '5-4' => 915, '5-5' => 950, 
+                '5-6' => 985, '5-7' => 1020, '5-8' => 1065, '5-9' => 1110,
+                
+                // Diğer dereceler için...
+                // 6. Derece
+                '6-1' => 760, '6-2' => 785, '6-3' => 810, '6-4' => 835, '6-5' => 870, 
+                '6-6' => 905, '6-7' => 915, '6-8' => 950, '6-9' => 985,
+                
+                // 7. Derece
+                '7-1' => 705, '7-2' => 720, '7-3' => 740, '7-4' => 760, '7-5' => 785, 
+                '7-6' => 810, '7-7' => 835, '7-8' => 870, '7-9' => 905,
+                
+                // 8. Derece
+                '8-1' => 660, '8-2' => 675, '8-3' => 690, '8-4' => 705, '8-5' => 720, 
+                '8-6' => 740, '8-7' => 760, '8-8' => 785, '8-9' => 810,
+                
+                // 9. Derece
+                '9-1' => 620, '9-2' => 630, '9-3' => 645, '9-4' => 660, '9-5' => 675, 
+                '9-6' => 690, '9-7' => 705, '9-8' => 720, '9-9' => 740,
+                
+                // 10. Derece
+                '10-1' => 590, '10-2' => 600, '10-3' => 610, '10-4' => 620, '10-5' => 630, 
+                '10-6' => 645, '10-7' => 660, '10-8' => 675, '10-9' => 690,
+                
+                // 11. Derece
+                '11-1' => 560, '11-2' => 570, '11-3' => 580, '11-4' => 590, '11-5' => 600, 
+                '11-6' => 610, '11-7' => 620, '11-8' => 630, '11-9' => 645,
+                
+                // 12. Derece
+                '12-1' => 545, '12-2' => 550, '12-3' => 555, '12-4' => 560, '12-5' => 570, 
+                '12-6' => 580, '12-7' => 590, '12-8' => 600, '12-9' => 610,
+                
+                // 13. Derece
+                '13-1' => 530, '13-2' => 535, '13-3' => 540, '13-4' => 545, '13-5' => 550, 
+                '13-6' => 555, '13-7' => 560, '13-8' => 570, '13-9' => 580,
+                
+                // 14. Derece
+                '14-1' => 515, '14-2' => 520, '14-3' => 525, '14-4' => 530, '14-5' => 535, 
+                '14-6' => 540, '14-7' => 545, '14-8' => 550, '14-9' => 555,
+                
+                // 15. Derece
+                '15-1' => 500, '15-2' => 505, '15-3' => 510, '15-4' => 515, '15-5' => 520, 
+                '15-6' => 525, '15-7' => 530, '15-8' => 535, '15-9' => 540
             );
+            
+            if (isset($default_gosterge_puanlari[$gosterge_puani_key])) {
+                $gosterge_puani = $default_gosterge_puanlari[$gosterge_puani_key];
+            } else {
+                return array(
+                    'success' => false,
+                    'message' => 'Gösterge puanı bulunamadı!',
+                );
+            }
         }
 
         // Katsayıları al
@@ -145,6 +255,27 @@ class Matas_Calculator {
                     $dil_tazminati = $dg['gosterge'] * $aylik_katsayi;
                     break;
                 }
+            }
+            
+            // Dil göstergesi bulunamadıysa varsayılan değerleri kullan
+            if ($dil_tazminati === 0) {
+                $dil_gosterge = 0;
+                
+                if ($dil_seviyesi === 'a' && $dil_kullanimi === 'evet') {
+                    $dil_gosterge = 1500;
+                } else if ($dil_seviyesi === 'b' && $dil_kullanimi === 'evet') {
+                    $dil_gosterge = 600;
+                } else if ($dil_seviyesi === 'c' && $dil_kullanimi === 'evet') {
+                    $dil_gosterge = 300;
+                } else if ($dil_seviyesi === 'a' && $dil_kullanimi === 'hayir') {
+                    $dil_gosterge = 750;
+                } else if ($dil_seviyesi === 'b' && $dil_kullanimi === 'hayir') {
+                    $dil_gosterge = 300;
+                } else if ($dil_seviyesi === 'c' && $dil_kullanimi === 'hayir') {
+                    $dil_gosterge = 150;
+                }
+                
+                $dil_tazminati = $dil_gosterge * $aylik_katsayi;
             }
         }
 
@@ -193,16 +324,27 @@ class Matas_Calculator {
                     break;
                 }
             }
+            
+            // Aile yardımı yoksa varsayılan değeri kullan
+            if ($aile_yardimi === 0) {
+                $aile_yardimi = 1200;
+            }
         }
 
         // Çocuk yardımı
         if ($cocuk_sayisi > 0) {
-            $normal_cocuk = $cocuk_sayisi - $cocuk_06 - $engelli_cocuk - $ogrenim_cocuk;
+            $normal_cocuk = max(0, $cocuk_sayisi - $cocuk_06 - $engelli_cocuk - $ogrenim_cocuk);
+            
+            // Varsayılan yardım tutarları
+            $normal_yardim = 150;
+            $yardim_06 = 300;
+            $yardim_engelli = 600;
+            $yardim_ogrenim = 250;
             
             // Normal çocuk yardımı
             foreach ($this->sosyal_yardimlar as $sy) {
                 if ($sy['tip'] === 'cocuk_normal') {
-                    $cocuk_yardimi += $normal_cocuk * $sy['tutar'];
+                    $normal_yardim = $sy['tutar'];
                     break;
                 }
             }
@@ -210,7 +352,7 @@ class Matas_Calculator {
             // 0-6 yaş çocuk yardımı
             foreach ($this->sosyal_yardimlar as $sy) {
                 if ($sy['tip'] === 'cocuk_0_6') {
-                    $cocuk_yardimi += $cocuk_06 * $sy['tutar'];
+                    $yardim_06 = $sy['tutar'];
                     break;
                 }
             }
@@ -218,7 +360,7 @@ class Matas_Calculator {
             // Engelli çocuk yardımı
             foreach ($this->sosyal_yardimlar as $sy) {
                 if ($sy['tip'] === 'cocuk_engelli') {
-                    $cocuk_yardimi += $engelli_cocuk * $sy['tutar'];
+                    $yardim_engelli = $sy['tutar'];
                     break;
                 }
             }
@@ -226,10 +368,16 @@ class Matas_Calculator {
             // Öğrenim çocuk yardımı
             foreach ($this->sosyal_yardimlar as $sy) {
                 if ($sy['tip'] === 'cocuk_ogrenim') {
-                    $cocuk_yardimi += $ogrenim_cocuk * $sy['tutar'];
+                    $yardim_ogrenim = $sy['tutar'];
                     break;
                 }
             }
+            
+            // Yardımları topla
+            $cocuk_yardimi = $normal_cocuk * $normal_yardim +
+                             $cocuk_06 * $yardim_06 +
+                             $engelli_cocuk * $yardim_engelli +
+                             $ogrenim_cocuk * $yardim_ogrenim;
         }
 
         // Kira yardımı
@@ -240,6 +388,11 @@ class Matas_Calculator {
                     break;
                 }
             }
+            
+            // Kira yardımı yoksa varsayılan değeri kullan
+            if ($kira_yardimi_tutari === 0) {
+                $kira_yardimi_tutari = 2000;
+            }
         }
 
         // Sendika yardımı
@@ -249,6 +402,11 @@ class Matas_Calculator {
                     $sendika_yardimi = $sy['tutar'];
                     break;
                 }
+            }
+            
+            // Sendika yardımı yoksa varsayılan değeri kullan
+            if ($sendika_yardimi === 0) {
+                $sendika_yardimi = 500;
             }
         }
 
@@ -269,7 +427,7 @@ class Matas_Calculator {
         // Asgari geçim indirimi hesapla
         $agi_tutari = 0;
         if ($asgari_gecim_indirimi) {
-            // Basitleştirilmiş AGİ hesaplaması - gerçek projede daha detaylı hesaplama yapılabilir
+            // Basitleştirilmiş AGİ hesaplaması
             $agi_orani = 0.50; // Bekar çalışan için temel oran
             
             if ($medeni_hal === 'evli' && $es_calisiyor === 'hayir') {
@@ -289,41 +447,38 @@ class Matas_Calculator {
             $agi_tutari = $asgari_ucret * $agi_orani * 0.15;
         }
         
-        // Gelir vergisini hesapla
+        // Gelir vergisini hesapla (düzeltilmiş)
         $gelir_vergisi = 0;
-        $vergi_dilimleri = $this->vergiler;
-        usort($vergi_dilimleri, function($a, $b) {
-            return $a['dilim'] - $b['dilim'];
-        });
-
         $kalan_matrah = $gelir_vergisi_matrahi;
+        $kumulatif_vergi = 0;
         
-        foreach ($vergi_dilimleri as $index => $dilim) {
+        // Vergi dilimlerini küçükten büyüğe sırala
+        usort($this->vergiler, function($a, $b) {
+            return intval($a['dilim']) - intval($b['dilim']);
+        });
+        
+        foreach ($this->vergiler as $index => $dilim) {
             $alt_limit = floatval($dilim['alt_limit']);
             $ust_limit = floatval($dilim['ust_limit']);
             $oran = floatval($dilim['oran']) / 100;
+// Son dilim veya sonsuz limit
+            if ($index === count($this->vergiler) - 1 || $ust_limit === 0) {
+                $gelir_vergisi += $kalan_matrah * $oran;
+                break;
+            }
             
-            if ($index === 0 && $kalan_matrah <= $ust_limit) {
-                // İlk dilim ve matrah bu dilimde bitiyor
-                $gelir_vergisi = $kalan_matrah * $oran;
+            // Matraha göre vergi hesapla
+            $hesaplanacak_matrah = min($kalan_matrah, $ust_limit - $alt_limit);
+            
+            if ($hesaplanacak_matrah <= 0) {
                 break;
-            } elseif ($index === 0) {
-                // İlk dilim ama matrah devam ediyor
-                $gelir_vergisi += $ust_limit * $oran;
-                $kalan_matrah -= $ust_limit;
-            } elseif ($index === count($vergi_dilimleri) - 1 || $ust_limit === 0) {
-                // Son dilim veya sınırsız dilim
-                $gelir_vergisi += $kalan_matrah * $oran;
+            }
+            
+            $gelir_vergisi += $hesaplanacak_matrah * $oran;
+            $kalan_matrah -= $hesaplanacak_matrah;
+            
+            if ($kalan_matrah <= 0) {
                 break;
-            } elseif ($kalan_matrah <= ($ust_limit - $alt_limit)) {
-                // Ara dilim ve matrah bu dilimde bitiyor
-                $gelir_vergisi += $kalan_matrah * $oran;
-                break;
-            } else {
-                // Ara dilim ve matrah devam ediyor
-                $dilim_farki = $ust_limit - $alt_limit;
-                $gelir_vergisi += $dilim_farki * $oran;
-                $kalan_matrah -= $dilim_farki;
             }
         }
         
